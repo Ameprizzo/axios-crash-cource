02ee8af67dd5f8ff78ec68efdf356d37a9d507d8
Amedeus Primi <amedeus926@gmail.com>
2023-03-16 12:58:41 +0300

    Post data

--- start.js
+++ start.js
@@ -1,19 +1,36 @@
 // GET REQUEST
 function getTodos() {
-  axios({
-    method: "get",
-    url: "https://jsonplaceholder.typicode.com/todos",
-    params: {
-      _limit: 5,
-    },
-  })
+  //   axios({
+  //     method: "get",
+  //     url: "https://jsonplaceholder.typicode.com/todos",
+  //     params: {
+  //       _limit: 5,
+  //     },
+  //   })
+  //     .then((res) => showOutput(res))
+  //     .catch((err) => console.error(err));
+  //
+
+  axios
+    .get("https://jsonplaceholder.typicode.com/todos", {
+      params: { _limit: 5 },
+    })
     .then((res) => showOutput(res))
     .catch((err) => console.error(err));
 }
 
 // POST REQUEST
 function addTodo() {
-  console.log("POST Request");
+  axios({
+    method: "post",
+    url: "https://jsonplaceholder.typicode.com/todos",
+    data: {
+      title: "New Todo",
+      completed: false,
+    },
+  })
+    .then((res) => showOutput(res))
+    .catch((err) => console.error(err));
 }
 
 // PUT/PATCH REQUEST
